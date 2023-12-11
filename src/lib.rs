613bf1a358735f77e125395c62cf55dd130620cb
Michał Pajda <gasosaur@gmail.com>
2023-12-11 11:29:45 +0100

    patch

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,25 @@ pub mod backend {
     use std::fs::read_to_string;
     use std::path::PathBuf;
 
-    pub fn head(path: PathBuf, n: usize) -> Vec<String> {
+    pub fn head(path: PathBuf, n: usize) -> Result<Vec<String>, String> {
+        Ok(
         read_to_string(path)
-            .unwrap()
+            .or(Err("Could not open file.".to_owned()))?
             .lines()
             .take(n)
             .map(|l| l.to_owned())
             .collect()
+        )
     }
 
-    pub fn tail(path: PathBuf, n: usize) -> Vec<String> {
+    pub fn tail(path: PathBuf, n: usize) -> Result<Vec<String>, String> {
         let lines = read_to_string(path)
-            .unwrap()
+            .or(Err("Could not open file.".to_owned()))?
             .lines()
             .map(|l| l.to_owned())
             .collect::<Vec<_>>();
         let len = lines.len();
-        lines[len - n..].iter().map(|l| l.to_owned()).collect()
+        Ok(lines[len - n..].iter().map(|l| l.to_owned()).collect())
     }
 }
 
@@ -28,12 +30,12 @@ mod tests {
     #[test]
     fn test_head() {
         let lines = head("files/test.txt".into(), 3);
-        assert_eq!(lines, vec!["1", "2", "3"]);
+        assert_eq!(lines, Ok(vec!["1".to_owned(), "2".to_owned(), "3".to_owned()]));
     }
 
     #[test]
     fn test_tail() {
         let lines = tail("files/test.txt".into(), 3);
-        assert_eq!(lines, vec!["8", "9", "10"]);
+        assert_eq!(lines, Ok(vec!["8".to_owned(), "9".to_owned(), "10".to_owned()]));
     }
 }
